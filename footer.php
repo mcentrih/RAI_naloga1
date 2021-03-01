d4116fedf466aaa25e775bb03d8bb1d8b6949fb4
Mcentrih <centrihmatjaz@gmail.com>
2021-03-01 11:30:17 +0100

    initial commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+<?php
+date_default_timezone_set('Europe/Ljubljana');
+?>
+<div style="overflow: hidden; position: fixed; bottom: 0; width: 99%; background-color: lightgray; border: 1px solid black; border-top: 5px solid black; margin-top: 10px">
+    <p style="float: left; padding-left: 10px"><?php echo date("d.m.Y h:i")?></p>
+    <p style="float: left; padding-left: 40%">RAI naloga 1</p>
+    <p style="float: right; padding-right: 10px;">Copyright by mcentrih RIT VS VP1</p>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include("header.php");
+?>
+
+<div style="background-color: lightgray; width: 75%; margin: 0 auto; padding: 10px; border: 1px solid black;">
+    <p>OGLAS</p>
+    <p>OGLAS</p>
+    <p>OGLAS</p>
+    <p>OGLAS</p>
+    <p>OGLAS</p>
+</div>
+
+
+<?php
+include("footer.php");
+?>
